f76949b1aaf1e4dfecc54e53459bd8cdcccd685b
Dnyaneshwar Mirge <dnyaneshwarmirge20@gmail.com>
2024-10-15 11:58:34 +0530

    l

--- pipeline/B38.groovy
+++ pipeline/B38.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage (pull){
             steps{
                 echo "we are pulling from github"
-                git "https://github.com/AnupDudhe/studentapp-ui.git"
+                git "https://github.com/AnupDudhe/studentapp-ui"
             }   
         }
         stage (build){
